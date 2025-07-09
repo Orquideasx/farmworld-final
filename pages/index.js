134ba02ab6b919cba85d117ddd5561b2ecc70e30
Orquideasx <58075984+Orquideasx@users.noreply.github.com>
2025-07-09 17:43:57 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import { useCallback } from 'react';
 
 const IDKitWidget = dynamic(
-  () => import('@worldcoin/idkit/react').then(mod => mod.IDKitWidget),
+  () => import { IDKitWidget } from '@worldcoin/idkit';).then(mod => mod.IDKitWidget),
   { ssr: false }
 );
 
